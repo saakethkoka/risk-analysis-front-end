598fc63c761257b51333275fa2828c39ecf3b8ac
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2022-11-12 15:51:49 -0600

    CREATED LOGIN PAGE LETS GOOOOO

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const base_url = "https://k6ijqv2sr8.execute-api.us-east-2.amazonaws.com/prod/";
+
+export const login = (username, password) => new Promise((resolve, reject) => {
+    const data = {
+        username: username,
+        password: password
+    }
+    axios.post(`${base_url}/login`, data)
+        .then(res => resolve(res))
+        .catch(err =>{
+            reject(err)
+        });
+});
